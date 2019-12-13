87a7a6bd588bcd14a775c8cc85228743c93fb17f
Christopher Dent <christopherdent01@gmail.com>
2019-12-13 02:15:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,12 +26,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/sessions/login' do
-
+    
     # the line of code below render the view page in app/views/sessions/login.erb
     erb :'sessions/login'
   end
 
   post '/sessions' do
+    puts params
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
       session[:user_id] = @user.id
